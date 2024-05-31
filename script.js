43c7eabf75a6668464511a7344bae8e8065a2b03
kevinjycui <kevinjycui@protonmail.com>
2024-05-31 01:30:07 -0400

    Website aesthetic improvements

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function closeMenu() {
     var menu = document.getElementById("menu")
     menu.style.display = "none";
 }
+
+function checkMenuScroll() {
+    var menu = document.getElementById("menu");
+    menu.style.opacity = 1 - window.scrollY/500;
+
+    if (menu.style.opacity <= 0)
+    {
+        document.getElementById("back-to-top").style.display = "block";
+        menu.style.pointerEvents = 'none';
+    }
+    else
+    {
+        document.getElementById("back-to-top").style.display = "none";
+        menu.style.pointerEvents = 'all';
+    }
+}
+
+window.onscroll = checkMenuScroll;
+window.onload = checkMenuScroll;
