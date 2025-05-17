0b7b059ed7e4007e17bea4bc0bbdc5eee61c712b
Kevin Junyang Cui <kevinjycui@protonmail.com>
2025-05-17 02:35:23 -0400

    Update website

--- contact/script.js
+++
@@ -1,7 +0,0 @@
-fetch("https://assets.mailerlite.com/jsonp/1047389/forms/128583801622234580/takel")
-
-function ml_webform_success_16910085() {
-    var $ = ml_jQuery || jQuery;
-    $('.row-success').show();
-    $('.row-form').hide();
-  }
